51608ac068ec7ad4e89aa6a5ebb9887e8985518d
Diego-Chang <diego_chang234@outlook.com>
2025-04-13 23:54:06 -0400

    added access modifier to classes.ts

--- taller-programacion-hibrida-unidad-1-por-diego-campusano/src/app/classes/classes.ts
+++ taller-programacion-hibrida-unidad-1-por-diego-campusano/src/app/classes/classes.ts
@@ -1,41 +1,41 @@
 export abstract class FiguraGeometrica { /*Clase abstracta FiguraGeometrica la funcionara como base para las demas figuras*/
-    nombre: string
-    constructor (nombre: string) {
+    private nombre: string
+    public constructor (nombre: string) {
         this.nombre = nombre
     }
 
-    abstract calcularPerimetro() : number
+    public abstract calcularPerimetro() : number
 }
 
 export class Circulo extends FiguraGeometrica { /*Clase Circulo que hereda de FiguraGeometrica*/
-    radio: number /*Variable usada en metodo y que sera recibida mediante Ionic Input. Representa el radio de un circulo*/
-    constructor (radio: number){
+    private radio: number /*Variable usada en metodo y que sera recibida mediante Ionic Input. Representa el radio de un circulo*/
+    public constructor (radio: number){
         super("Circulo")
         this. radio = radio
     }
-    override calcularPerimetro(): number { /*Sobreescrive metodo abstracto calcularPerimetro. Calcula el perimetro de un circulo en base a un radio dado*/
+    public override calcularPerimetro(): number { /*Sobreescrive metodo abstracto calcularPerimetro. Calcula el perimetro de un circulo en base a un radio dado*/
         return this.radio * 2 * 3.1416
     }
 }
 
 export class TrianguloEscaleno extends FiguraGeometrica { /*Clase TrianguloEscaleno que hereda de FiguraGeometrica*/
-    ladoA: number /*Lineas 22, 23, y 24 son variables usadas en metodo y que seran recibidas mediante Ionic Input. Representan los lados de un triangulo*/
-    ladoB: number
-    ladoC: number
+    private ladoA: number /*Lineas 22, 23, y 24 son variables usadas en metodo y que seran recibidas mediante Ionic Input. Representan los lados de un triangulo*/
+    private ladoB: number
+    private ladoC: number
 
-    constructor (ladoA: number, ladoB: number, ladoC: number) {
+    public constructor (ladoA: number, ladoB: number, ladoC: number) {
         super("Triangulo Escaleno")
         this.ladoA = ladoA
         this.ladoB = ladoB
         this.ladoC = ladoC
     }
-    override calcularPerimetro(): number { /*Sobreescrive metodo abstracto calcularPerimetro. Calcula el perimetro de un triangulo en base a los lados dados*/
+    public override calcularPerimetro(): number { /*Sobreescrive metodo abstracto calcularPerimetro. Calcula el perimetro de un triangulo en base a los lados dados*/
         return this.ladoA + this.ladoB + this.ladoC
     }
 }
 
 export class TrianguloEquilatero extends TrianguloEscaleno { /*Clase TrianguloEquilatero que herda de TrianguloEscaleno*/
-    constructor (ladoA: number) {
+    public constructor (ladoA: number) {
         super(ladoA, ladoA, ladoA)
     }
 }
